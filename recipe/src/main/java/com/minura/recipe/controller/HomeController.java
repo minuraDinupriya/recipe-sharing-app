e9aabc5b18924ce1130b9daa9ee0e9eb3e41c895
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-11 16:25:38 +0530

    set project environment

---
+++ recipe/src/main/java/com/minura/recipe/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.minura.recipe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String homeController(){
+        return "Hello";
+    }
+
+//    @PostMapping
+}
