b1fe910905290a3cefcd60596d8d89000cbfe491
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-15 07:32:59 +0530

    create delete api

--- recipe/src/main/java/com/minura/recipe/controller/UserController.java
+++ recipe/src/main/java/com/minura/recipe/controller/UserController.java
@@ -3,16 +3,14 @@ package com.minura.recipe.controller;
 import com.minura.recipe.model.User;
 import com.minura.recipe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    private final UserRepository userRepository;//
+    private final UserRepository userRepository;
     @PostMapping("users")
     public String createUser(@RequestBody User user)    {
         Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
@@ -22,5 +20,16 @@ public class UserController {
         }
         userRepository.save(user);
         return "User saved successfully";
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public String deleteUser(@PathVariable Long userId){
+        Optional<User> optionalUser = userRepository.findById(userId);
+        System.out.println(optionalUser);
+        if (optionalUser.isPresent()){
+            userRepository.delete(optionalUser.get());
+            return "User with id "+userId+" is deleted";
+        }
+        return "Invalid userId";
     }
 }
