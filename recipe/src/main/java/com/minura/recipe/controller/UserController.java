495cfd6bab53891da5c373320e347fb144382c36
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-15 09:37:48 +0530

    get all user api created

--- recipe/src/main/java/com/minura/recipe/controller/UserController.java
+++ recipe/src/main/java/com/minura/recipe/controller/UserController.java
@@ -3,15 +3,17 @@ package com.minura.recipe.controller;
 import com.minura.recipe.model.User;
 import com.minura.recipe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
-    @PostMapping("users")
+    @PostMapping("/add-user")
     public String createUser(@RequestBody User user)    {
         Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
         System.out.println(optionalUser);
@@ -22,7 +24,7 @@ public class UserController {
         return "User saved successfully";
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable Long userId){
         Optional<User> optionalUser = userRepository.findById(userId);
         System.out.println(optionalUser);
@@ -31,5 +33,10 @@ public class UserController {
             return "User with id "+userId+" is deleted";
         }
         return "Invalid userId";
+    }
+
+    @GetMapping("/get-users")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
